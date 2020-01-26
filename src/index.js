773b246fa6f47b1bb8398edfd16f45c452f6b230
Stephen Kastona <stephenkastona@gmail.com>
2020-01-26 20:55:09 +0100

    redirected

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 const PORT = process.env.PORT || 3000
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    console.log(req.body)
+    res.send('testing')
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`Listening on ${PORT}`)
---
+++ src/models/user.js
@@ -0,0 +1,5 @@
+
+
+const User = mongoose.model('User', {
+    name: 
+})
