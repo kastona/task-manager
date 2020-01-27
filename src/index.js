8a42940eceba9665dedd3f6feb7d24f2a62b6bd7
Stephen Kastona <stephenkastona@gmail.com>
2020-01-27 15:22:57 +0100

    reading functions added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
 const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-    console.log(req.body)
-    res.send('testing')
+    
+
+    
+    const user = new User(req.body)
+    user.save().then(data =>{
+        res.status(201).send(data)
+    }).catch(error => {
+        res.status(400)
+        res.send(error)
+    })
+})
+
+app.get('/users', (req, res) => {
+    User.find({}).then(users => {
+        if(!users) {
+            return res.status(400).send()
+        }
+        res.send(users)
+    }).catch(error => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id).then(user => {
+        if(!user) {
+            return res.status(400).send()
+        }
+
+        res.send(user)
+
+    }).catch(error => {
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(t => {
+        res.status(201).send(t)
+    }).catch(error => {
+        res.status(400).send(error)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then(tasks => {
+        if(!tasks) {
+            return res.status(400).send()
+        }
+
+        res.send(tasks)
+    }).catch(error => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findOne({_id: req.params.id}).then(task => {
+        if(!task) {
+            return res.status(400).send()
+        }
+
+        res.send(task)
+    }).catch(error => {
+        res.status(500).send()
+    })
 })
 
 app.listen(PORT, (req, res) => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,44 @@
+const validator = require('validator')
+const mongoose = require('mongoose')
 
 
 const User = mongoose.model('User', {
-    name: 
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true, 
+        validate(pass) {
+            if(pass.includes('password')) {
+                throw new Error('Password not secure!')
+            }
+        }
+    },
+
+    age: {
+        type: Number,
+        validate(age) {
+            if(age <0) {
+                throw new Error('Age cannot be negative!')
+            }
+        }
+    }
 })
+
+module.exports = User
