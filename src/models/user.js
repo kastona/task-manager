97110177ad4ae131530eec33dcf77db0000d8c9a
Stephen Kastona <stephenkastona@gmail.com>
2020-02-03 21:35:44 +0100

    login and signup with tokens implemented

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const validator = require('validator')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -12,6 +13,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         trim: true,
+        unique: true,
         lowercase: true,
         validate(value) {
             if(!validator.isEmail(value)) {
@@ -38,8 +40,39 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age cannot be negative!')
             }
         }
-    }
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 })
+
+userSchema.statics.findByCredentials = async (email, password) =>{
+    const user = await User.findOne({email})
+
+    if(!user) {
+        throw new Error('Unable to login!')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) {
+        throw new Error('Unable to login!')
+    }
+
+    return user;
+}
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()}, 'something')
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
 
 userSchema.pre('save', async function (next) {
     const user = this
@@ -47,8 +80,6 @@ userSchema.pre('save', async function (next) {
     const isModified = user.isModified('password');
     if(isModified) {
         user.password = await bcrypt.hash(user.password, 8)
-
-        console.log(user.password)
     }
     next()
 })
--- src/routers/user.js
+++ src/routers/user.js
@@ -9,11 +9,23 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     }catch (err) {
         res.status(400).send(err)
     }
     
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        await user.save()
+        res.send({user,token})
+    }catch(err) {
+        res.status(400).send()
+    }
 })
 
 router.get('/users', async (req, res) => {
